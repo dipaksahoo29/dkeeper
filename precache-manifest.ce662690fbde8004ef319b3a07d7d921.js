e4e5d1b412d32b6f75eae7c461ce017f4fadee85
Dipak Sahoo <dipak.sahoo29@gmail.com>
2020-09-05 01:20:52 +0530

    Updates

---
+++ precache-manifest.ce662690fbde8004ef319b3a07d7d921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2650944b780b3f4cef2616da0de827f2",
+    "url": "/dkeeper/index.html"
+  },
+  {
+    "revision": "f589952c609558d92834",
+    "url": "/dkeeper/static/css/main.46d9e476.chunk.css"
+  },
+  {
+    "revision": "a69f30ede89a0c49f94e",
+    "url": "/dkeeper/static/js/2.4ca2be1e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dkeeper/static/js/2.4ca2be1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f589952c609558d92834",
+    "url": "/dkeeper/static/js/main.90595c32.chunk.js"
+  },
+  {
+    "revision": "19ee865074208c52358e",
+    "url": "/dkeeper/static/js/runtime-main.f96294ee.js"
+  }
+]);
